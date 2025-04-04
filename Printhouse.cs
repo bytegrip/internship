e7efe2f8a30e00eb9d9cd8275e977e3a829149d2
lumijiez <59575049+lumijiez@users.noreply.github.com>
2025-04-05 01:29:45 +0300

    Create Printhouse representation

--- Printhouse.cs
+++ Printhouse.cs
@@ -2,5 +2,29 @@ namespace Internship;
 
 public class Printhouse
 {
-    
+    private string name;
+    private int activePrinters;
+
+    public string Name 
+    { 
+        get { return name; } 
+        private set { name = value; } 
+    }
+
+    public int ActivePrinters 
+    { 
+        get { return activePrinters; } 
+        set { activePrinters = value > 0 ? value : 1; } 
+    }
+
+    public Printhouse(string name, int activePrinters)
+    {
+        this.name = name;
+        this.activePrinters = activePrinters > 0 ? activePrinters : 1;
+    }
+
+    public double CalculateTotalCost(double discount)
+    {
+        return CalculateTotalCost() * (1 - discount);
+    }
 }

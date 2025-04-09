07eec4d04f639d73907ed14a84869f57bf8361ce
lumijiez <59575049+lumijiez@users.noreply.github.com>
2025-04-10 00:41:35 +0300

    Add shipping cost to materials

--- PrintMaterials/PrintMaterial.cs
+++ PrintMaterials/PrintMaterial.cs
@@ -2,13 +2,16 @@ using Internship.PrintJobs;
 
 namespace Internship.PrintMaterials;
 
-public abstract class PrintMaterial(string type, PaperSize size, double pricePerUnit)
+public abstract class PrintMaterial(MaterialTypes type, PaperSize size, double pricePerUnit)
 {
-    public string Type { get; } = type;
-
+    public MaterialTypes Type { get; } = type;
     public PaperSize Size { get; } = size;
-
     protected double PricePerUnit { get; } = pricePerUnit;
 
     public abstract double CalculateCost(int quantity);
+    
+    public virtual double GetShippingCost(int quantity = 1, double baseRate = 5.0)
+    {
+        return baseRate + (quantity * 0.1);
+    }
 }
